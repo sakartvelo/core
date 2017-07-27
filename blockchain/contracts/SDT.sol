d1b9e8950a427a7c4bd1ee73566cf02045c3d91c
Antony Shchukin <szwtdy@gmail.com>
2017-07-27 13:26:51 +0300

    add all contracts to project

---
+++ blockchain/contracts/SDT.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.4;
+
+
+import "./zeppelin/token/StandardToken.sol";
+import "./zeppelin/ownership/Ownable.sol";
+
+
+/*
+ *  Sonm Dummy Token for test
+*
+ *  
+ *
+ * Very simple ERC20 Token example, where all tokens are pre-assigned
+ * to the creator. Note they can later distribute these tokens
+ * as they wish using `transfer` and other `StandardToken` functions.
+ *
+ *  simple standard
+ */
+contract SDT is StandardToken, Ownable {
+
+    string public name = "Sonm Dummy Token";
+
+    string public symbol = "SDT";
+
+    uint public decimals = 18;
+
+    uint public INITIAL_SUPPLY = 1000000 * 1 ether;
+
+    function SDT() {
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+    }
+
+    function mintToken(address target, uint256 mintedAmount) onlyOwner {
+        balances[target] += mintedAmount;
+        totalSupply += mintedAmount;
+        Transfer(0, owner, mintedAmount);
+        Transfer(owner, target, mintedAmount);
+    }
+
+}
